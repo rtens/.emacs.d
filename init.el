a7395f6ac0d02372045c1df50d011f4315f7e92b
Nikolas Martens <nikolas.m@rtens.org>
2024-09-15 12:05:25 +0200

    MELPA and co

--- init.el
+++ init.el
@@ -2,9 +2,11 @@
 (setq inhibit-splash-screen t)
 (setq root-directory default-directory)
 (winner-mode)
+(electric-pair-mode)
 
-;; ========== formatting ===========
+;; ========== javascrpt ============
 (setq js-indent-level 2)
+;; (add-hook 'js-mode-hook #'display-line-numbers-mode)
 
 ;; ======== desktop mode ===========
 (desktop-save-mode 1)
@@ -42,3 +44,32 @@
 			 "./.emacs-local.el"))
   (if (file-exists-p init-file)
       (load-file init-file)))
+
+;; ========== packages ============
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(use-package move-dup :ensure t)
+(global-move-dup-mode)
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+(flycheck-add-mode 'javascript-eslint 'js-mode)
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+(provide 'init)
+;;; init.el ends here
