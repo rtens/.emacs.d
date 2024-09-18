77edd34e2dbe61d0837b4f83ba02d0e6dadfcf08
Nikolas Martens <Nikolas.M@rtens.org>
2024-09-18 11:57:21 +0200

    User windows key as super

--- setup-system-windows.el
+++ setup-system-windows.el
@@ -1,2 +1,7 @@
+;; Use windows key as super
+(setq w32-pass-lwindow-to-system nil
+      w32-lwindow-modifier 'super)
+(w32-register-hot-key [s-])
+
 ;; Use a nice font
 (set-frame-font "DejaVu Sans Mono-10")
