b4432947eff7fcd7ab22f0584a418e42d55aecfa
Nikolas Martens <Nikolas.M@rtens.org>
2024-09-19 14:29:52 +0200

    Whitespace...

--- setup-appearance.el
+++ setup-appearance.el
@@ -10,25 +10,25 @@
 
 ;; Center buffer
 (use-package centered-window
-  :ensure t
-  :config (centered-window-mode t))
+	:ensure t
+	:config (centered-window-mode t))
 
 ;; Make it dark
 (use-package darcula-theme
-  :ensure t
-  :config (load-theme 'darcula))
+	:ensure t
+	:config (load-theme 'darcula))
 
 ;; Fix for not splitting right with wide margins
 (defun my-split-window-sensibly (&optional window)
-    (centered-window-mode 0)
-    (unwind-protect
+		(centered-window-mode 0)
+		(unwind-protect
 	(split-window-sensibly window)
-      (centered-window-mode 1)))
+			(centered-window-mode 1)))
 (setq split-window-preferred-function #'my-split-window-sensibly)
 
 (defun my-split-window-right ()
-  (interactive)
-  (centered-window-mode 0)
-  (split-window-right)
-  (centered-window-mode 1))
+	(interactive)
+	(centered-window-mode 0)
+	(split-window-right)
+	(centered-window-mode 1))
 (global-set-key (kbd "C-x 3") 'my-split-window-right)
