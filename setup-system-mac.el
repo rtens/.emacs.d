25c77acc04507e824b190b604d55628d5d659b16
Nikolas Martens <nikolas@theclimatechoice.com>
2024-09-23 16:56:56 +0200

    No more super

--- setup-system-mac.el
+++ setup-system-mac.el
@@ -1,11 +1,7 @@
 ;; Make modifiers work as C, s, M
 (setq
  ns-control-modifier 'control
- ns-option-modifier 'super
  ns-command-modifier 'meta)
-
-;; Use the right alt for special characters
-(setq ns-right-alternate-modifier 'none)
 
 ;; Use a nice font
 (set-frame-font "Menlo-14")
--- setup-system-windows.el
+++ setup-system-windows.el
@@ -1,6 +1,2 @@
-;; Use windows key as super (map capslock to apps key)
-;; In AutoHotKey with #IfWinActive, emacs { Capslock::AppsKey }
-(setq w32-apps-modifier 'super)
-
 ;; Use a nice font
 (set-frame-font "DejaVu Sans Mono-10")
