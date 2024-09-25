57ea08e3057474e3e57e3239c9e4b56a5b8706cf
Nikolas Martens <nikolas@theclimatechoice.com>
2024-09-25 13:45:35 +0200

    Move setup files to own folder

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (defun setup (file)
 	"Import a file from the emacs directory."
 	(load-file (expand-file-name
-				(concat "setup-" file ".el")
+				(concat "setup/" file ".el")
 				user-emacs-directory)))
 
 ;; Turn off the lights
